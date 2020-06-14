4c986b3653708a348318165a4efbdbb6804aa197
julio cesar <vcjuliocesar@outlook.com>
2020-06-14 16:36:01 -0500

    show an array

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
           Learn React
         </a>
         <section>
-          <MiComponente/>
+          <MiComponente></MiComponente>
         </section>
       </header>
     </div>
--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-class MiComponente extends React.Component{
+class MiComponente extends Component{
 
     render(){
+        let receta = {
+            nombre:"Pizza",
+            ingredientes:["Tomate","Queso","Pepperoni"],
+            calorias:400
+        };
         return (
-            <h1>Hola soy un componente llamado: MiComponente</h1>
+            <div className="mi-componente">
+                <h1>{'Reseta' + receta.nombre}</h1>
+                <h2>{'Calorias' + receta.calorias}</h2>
+                <ol>
+                    {
+                        receta.ingredientes.map((ingrediente,i) => {
+                            return (
+                                <li key={i}>
+                                    {ingrediente}
+                                </li>
+                            );
+                        })
+                    }
+                </ol>
+            </div>
         );
     }
 }
