077952c91af07778b690a96397ee1a881186585f
julio cesar <vcjuliocesar@outlook.com>
2020-06-19 22:57:55 -0500

    estados de los componentes

--- src/components/SeccionPruebas.js
+++ src/components/SeccionPruebas.js
@@ -4,23 +4,42 @@ import MiComponente from "./MiComponente";
 import Peliculas from "./Peliculas";
 
 class SeccionPruebas extends Component {
+  contador = 0;
+
+  state = {
+    contador:0
+  };
+  
+  HolaMundo(nombre) {
+    let presentacion = (
+      <div>
+        <h1>Hola, soy {nombre}</h1>
+      </div>
+    );
+    return presentacion;
+  }
+
+  sumar = (e) => {
+    this.setState({
+      contador:(this.state.contador + 1)
+    });
+  }
+
+  restar = (e) => {
+    this.setState({
+      contador:(this.state.contador - 1)
+    });
+  }
+  
   render() {
     let nombre = "Julio Cesar";
-    function HolaMundo(nombre) {
-      let presentacion = (
-        <div>
-          <h1>Hola, soy {nombre}</h1>
-        </div>
-      );
-      return presentacion;
-    }
     return (
       <section id="content">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        {HolaMundo(nombre)}
+        {this.HolaMundo(nombre)}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -32,6 +51,14 @@ class SeccionPruebas extends Component {
         <section className="componentes">
           <MiComponente />
           <Peliculas></Peliculas>
+          <h2 className="subheader">Estado</h2>
+          <p>
+            Contado: {this.state.contador}
+          </p>
+          <p>
+            <input type="button" value="Sumar" onClick={this.sumar}/>
+            <input type="button" value="Restar" onClick={this.restar}/>
+          </p>
         </section>
       </section>
     );
