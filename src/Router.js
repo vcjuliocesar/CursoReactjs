ed8cbb93ebd5007e12a8138477d3da76a726fba7
julio cesar <vcjuliocesar@outlook.com>
2020-07-02 09:14:50 -0500

    pagina de error

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Peliculas from './components/Peliculas';
 import SeccionPruebas from './components/SeccionPruebas';
 import MiComponente from './components/MiComponente';
+import Error from './components/Error';
 
 class Router extends Component{
     render(){
@@ -13,6 +14,7 @@ class Router extends Component{
                     <Route exact path = "/" component = {Peliculas}/>
                     <Route exact path = "/ruta-prueba" component = {SeccionPruebas}/>
                     <Route exact path = "/segunda-ruta" component = {MiComponente}/>
+                    <Route component={Error}/>
                 </Switch>
             </BrowserRouter>
         );
---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <section id="content">
+        <h2>Pagina no encontrada</h2>
+        <p>La pagina no existe</p>
+    </section>
+  );
+};
+
+export default Error;
