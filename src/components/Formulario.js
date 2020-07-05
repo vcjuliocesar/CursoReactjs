a0c270281a940591c66aa8856ce94aadfa01fc0b
julio cesar <vcjuliocesar@outlook.com>
2020-07-05 13:57:14 -0500

    Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,15 +3,59 @@ import Slider from "./Slider";
 import Sidebar from "./Sidebar";
 
 class Formulario extends Component {
+    
+    nombreRef = React.createRef();
+    apellidosRef = React.createRef();
+    bioRef = React.createRef();
+    generoHombreRef = React.createRef();
+    generoMujerRef = React.createRef();
+    generoOtroRef = React.createRef();
+
+    recibirFormulario = (e) => {
+        e.preventDefault();
+        console.log("Formulario enviado");
+        console.log(this.nombreRef.current.value);
+        console.log(this.apellidosRef.current.value);
+        console.log(this.bioRef.current.value);
+        console.log(this.generoHombreRef.current.value);
+        console.log(this.generoMujerRef.current.value);
+        console.log(this.generoOtroRef.current.value);
+    }
+
   render() {
     return (
       <div id="formulario">
-        <Slider title="Formulario" size="slider-small"/>
         <div className="center">
           <div id="content">
-            
+            <h1 className="subheader">Formulario</h1>
+            <form className="mid-form" onSubmit={this.recibirFormulario}>
+              <div className="form-group">
+                <label htmlFor="nombre">Nombre</label>
+                <input type="text" name="nombre" ref={this.nombreRef}/>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="apellidos">Apellidos</label>
+                <input type="text" name="apellidos" ref={this.apellidosRef}/>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="bio">Biografia</label>
+                <textarea name="bio" ref={this.bioRef}></textarea>
+              </div>
+
+              <div className="form-group radibuttons">
+                <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/> Hombre
+                <input type="radio" name="genero" value="mujer" ref={this.generoMukerRef}/> Mujer
+                <input type="radio" name="genero" value="otro" ref={this.generoOtroRef}/> Otro
+              </div>
+
+              <div className="clearfix"></div>
+
+              <input type="submit" value="Enviar" className="btn btn-success" />
+            </form>
           </div>
-          <Sidebar blog="false"/>
+          <Sidebar blog="false" />
         </div>
       </div>
     );
